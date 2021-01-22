f3ed06007a221c3fa6e0716275ea4c00f5830824
Ayesha Shabbir <ayeshashabbir@tenpearls.com>
2021-01-22 17:15:31 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+import { BrowserRouter } from "react-router-dom"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./App.css";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {MyContext} from './context'
+import TodoContainer from "./components/TodoContainer"
+
+const element = <h1>Hello from Create React App</h1>
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
